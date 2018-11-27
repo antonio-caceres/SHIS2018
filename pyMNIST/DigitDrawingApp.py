0c5a8d109efc3b2339854d93bd2dad49c4919305
Antonio Caceres <acaceres19@shschools.org>
2018-11-27 13:38:00 -0800

    Reprogrammed the Digit Drawing App
    Organized the Digit Drawing App.

--- pyMNIST/DigitDrawingApp.py
+++ pyMNIST/DigitDrawingApp.py
@@ -1,79 +1,33 @@
-# created by Mr. V, for Antonio, as a quick-and-sort-of-dirty image editor
-# extremely simple image editor for 28x28 black-and-white images
-# to install pygame:
-#   sudo pip install pygame
-# if you need pip: curl https://bootstrap.pypa.io/get-pip.py > get-pip.py
+"""
+An extremely simple image editor for 28x28 blackk-and-white images, created by Mr. V for Antonio.
+Much of the code used in this project is from https://inventwithpython.com/pygameHelloWorld.py
+Install pygame using the Terminal:
+    sudo pip install pygame
+Install pip using the Terminal:
+    curl https://bootstrap.pypa.io/get-pip.py > get-pip.py
+"""
+import pygame, sys, os, random
+from pygame.locals import *  # for QUIT and other state/type info
 
-# much of this code is from https://inventwithpython.com/pygameHelloWorld.py
-import os
-import pygame, sys
-from pygame.locals import * # for QUIT, and other state/type info
+# Drawing Area Constants
+HEIGHT, WIDTH = 28, 28
+SIZE = (10, 10)
+OFFSET = (20, 20)
+MAX_VALUES = 30  # will determine how quickly the brush paints black
+BRUSH = [" XX ",
+         "XXXX",
+         " XX "]
 
-# set up pygame
-pygame.init()
-
-# set up the window
-windowSurface = pygame.display.set_mode((500, 400), 0, 24)
-pygame.display.set_caption('Hello world!')
-
-# pygame.mouse.set_cursor(*pygame.cursors.arrow)
-
-# set up the colors
+# Color Constants
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
-# set up fonts
-font_size = 48
-basicFont = pygame.font.SysFont(None, font_size)
 
-# set up the text
-text = basicFont.render('esc to clear - enter to save', True, BLACK, WHITE)
-textRect = text.get_rect()
-textRect.centerx = windowSurface.get_rect().centerx
-textRect.y = windowSurface.get_rect().height - font_size;
-
-# draw the white background onto the surface
-windowSurface.fill(WHITE)
-
-# draw a green polygon onto the surface
-# pygame.draw.polygon(windowSurface, GREEN, ((146, 0), (291, 106), (236, 277), (56, 277), (0, 106)))
-
-# draw some blue lines onto the surface
-# pygame.draw.line(windowSurface, BLUE, (60, 60), (120, 60), 4)
-# pygame.draw.line(windowSurface, BLUE, (120, 60), (60, 120))
-# pygame.draw.line(windowSurface, BLUE, (60, 120), (120, 120), 4)
-
-# draw a blue circle onto the surface
-# pygame.draw.circle(windowSurface, BLUE, (300, 50), 20, 0)
-
-# draw a red ellipse onto the surface
-# pygame.draw.ellipse(windowSurface, RED, (300, 250, 40, 80), 1)
-
-# draw the text's background rectangle onto the surface
-# pygame.draw.rect(windowSurface, RED, (textRect.left-20, textRect.top-20, textRect.width+40, textRect.height+40))
-
-# get a pixel array of the surface
-# pixArray = pygame.PixelArray(windowSurface)
-# pixArray[480][380] = BLACK
-# del pixArray
-
-# draw the text onto the surface
-windowSurface.blit(text, textRect)
-
-
-def rc():  # random color
-    import random
-    return (random.randint(0,255),random.randint(0,255),random.randint(0,255))
-
-
-height, width = 28, 28
-size = (10,10)
-offset = (20,20)
-MAX_VALUES = 12
-values = None # will be an array of integers, from 0 to MAXVALUES
+def get_random_color():
+    return random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
 
 
 def new_2d_values(height, width):
@@ -85,12 +39,11 @@ def new_2d_values(height, width):
     return values
 
 
-values = new_2d_values(height, width)
-
-
 def value_to_color(v):
-    if v <= 0: return WHITE
-    if v > MAX_VALUES: v = MAX_VALUES
+    if v <= 0:
+        return WHITE
+    if v > MAX_VALUES:
+        v = MAX_VALUES
     v = int(float(MAX_VALUES - v) / MAX_VALUES * 255)
     return v, v, v
 
@@ -98,35 +51,33 @@ def value_to_color(v):
 def draw_raster(values, offset, size):
     height, width = len(values), len(values[0])
     rect_border = 1
-    pygame.draw.rect(windowSurface, BLACK, (
-        offset[0]-rect_border,
-        offset[1]-rect_border,
-        width*size[0]+rect_border*2,
-        height*size[1]+rect_border*2), rect_border)
+    pygame.draw.rect(windowSurface, BLACK,
+                     (offset[0] - rect_border,
+                      offset[1] - rect_border,
+                      width * size[0] + rect_border * 2,
+                      height * size[1] + rect_border * 2),
+                     rect_border)
     for r in range(height):
         for c in range(width):
-            x, y = offset[0]+size[0]*c, offset[1]+size[1]*r
+            x, y = offset[0] + size[0] * c, offset[1] + size[1] * r
             color = value_to_color(values[r][c])
-            pygame.draw.rect(windowSurface, color,
-                #(textRect.left - 20, textRect.top - 20, textRect.width + 40, textRect.height + 40)
-                (x, y, size[0], size[1])
-            )
+            pygame.draw.rect(windowSurface, color, (x, y, size[0], size[1]))
 
 
 def get_raster_position(mouse, offset, size):
-    return (float(mouse[0]-offset[0])/size[0], float(mouse[1]-offset[1])/size[1])
+    return float(mouse[0] - offset[0]) / size[0], float(mouse[1] - offset[1]) / size[1]
 
 
-def create_and_save_image(values, imageName):
+def create_and_save_image(values, image_name):
     height, width = len(values), len(values[0])
     surf = pygame.Surface((width, height))
     surf.fill(WHITE)
-    pixArray = pygame.PixelArray(surf)
+    pix_array = pygame.PixelArray(surf)
     for r in range(height):
         for c in range(width):
-            pixArray[c][r] = value_to_color(values[r][c]) # note: pygame pixel maps are x/y not row/col
-    del pixArray
-    pygame.image.save(surf, imageName)
+            pix_array[c][r] = value_to_color(values[r][c])  # note: pygame pixel maps are x/y not row/col
+    del pix_array
+    pygame.image.save(surf, image_name)
 
 
 def get_complete_title(base_title):
@@ -143,53 +94,81 @@ def get_complete_title(base_title):
     return complete_title
 
 
-# draw the window onto the screen
+def add_to_values_at(values, p):
+    if 0 <= p[0] < WIDTH and 0 <= p[1] < HEIGHT:
+        r,c = int(p[1]),int(p[0])
+        up,dn,lf,rt = p[1]-r < .5, p[1]-r > .5,p[0]-c < .5,p[0]-c > .5
+        values[r][c] += 5
+        if r > 0 and up:
+            values[r-1][c] += 2
+            if c > 0 and lf: values[r-1][c-1] += 1
+            if c > WIDTH-1 and rt:values[r-1][c+1] += 1
+        if c > 0 and lf:
+            values[r][c-1] += 2
+        if r < HEIGHT-1 and dn:
+            values[r+1][c] += 2
+            if c > 0 and lf: values[r+1][c-1] += 1
+            if c > WIDTH-1 and rt:values[r+1][c+1] += 1
+        if c > WIDTH-1 and rt:
+            values[r][c+1] += 2
+
+
+def add_brush_to_values_at(values, rasterPos, brush):
+    for r in range(len(brush)):
+        for c in range(len(brush[r])):
+            if brush[r][c] != ' ':
+                add_to_values_at(values, (rasterPos[0] + c, rasterPos[1] + r))
+
+
+pygame.init()  # set up pygame
+windowSurface = pygame.display.set_mode((500, 400), 0, 24)  # set up the window
+pygame.display.set_caption("SHIS Digit Drawing")
+
+# set up fonts
+font_size = 48
+basic_font = pygame.font.SysFont(None, font_size)
+
+# set up the text
+text = basic_font.render('esc to clear - enter to save', True, BLACK, WHITE)
+text_rect = text.get_rect()
+text_rect.centerx = windowSurface.get_rect().centerx
+text_rect.y = windowSurface.get_rect().height - font_size;
+
+windowSurface.fill(WHITE)  # draw the white background onto the surface
+windowSurface.blit(text, text_rect)  # draw the text onto the surface
+
+values = new_2d_values(HEIGHT, WIDTH)
+
 pygame.display.update()
 
 clicking = False
-rasterMustRedraw = True
-# run the game loop
+raster_must_redraw = True
+
 while True:
     for event in pygame.event.get():
         # print(event)
         if event.type == QUIT:
-            create_and_save_image(values, get_complete_title("Image"))
             pygame.quit()
             sys.exit()
         elif event.type == KEYDOWN:
-            if event.key == 13 or event.key == 32: # save on space or return
-                create_and_save_image(values, get_complete_title("Image"))
-            if event.key == 27 or event.key == 8: # reset on escape or backspace
-                values = new_2d_values(height, width)
-                rasterMustRedraw = True
+            if event.key == 13 or event.key == 32:  # save on space or return
+                create_and_save_image(values, get_complete_title("Image "))
+            if event.key == 27 or event.key == 8:  # reset on escape or backspace
+                values = new_2d_values(HEIGHT, WIDTH)
+                raster_must_redraw = True
         elif event.type == MOUSEBUTTONDOWN:
             clicking = True
+            rasterPos = get_raster_position(event.pos, OFFSET, SIZE)
+            add_brush_to_values_at(values, rasterPos, BRUSH)
+            raster_must_redraw = True
         elif event.type == MOUSEBUTTONUP:
             clicking = False
         elif event.type == MOUSEMOTION:
             if clicking:
-                rasterPos = get_raster_position(event.pos, offset, size)
-                if (rasterPos[0] >= 0 and rasterPos[0] < width 
-                and rasterPos[1] >= 0 and rasterPos[1] < height):
-                    r,c = int(rasterPos[1]),int(rasterPos[0])
-                    up,dn,lf,rt = rasterPos[1]-r < .5, rasterPos[1]-r > .5,rasterPos[0]-c < .5,rasterPos[0]-c > .5
-                    values[r][c] += 5
-                    if r > 0 and up:
-                        values[r-1][c] += 2
-                        if c > 0 and lf: values[r-1][c-1] += 1
-                        if c > width-1 and rt:values[r-1][c+1] += 1
-                    if c > 0 and lf:
-                        values[r][c-1] += 2
-                    if r < height-1 and dn:
-                        values[r+1][c] += 2
-                        if c > 0 and lf: values[r+1][c-1] += 1
-                        if c > width-1 and rt:values[r+1][c+1] += 1
-                    if c > width-1 and rt:
-                        values[r][c+1] += 2
-                    rasterMustRedraw = True
-        if rasterMustRedraw:
-            draw_raster(values, offset, size)
-            # draw the window onto the screen
-            pygame.display.update()
-    rasterMustRedraw = False
-
+                rasterPos = get_raster_position(event.pos, OFFSET, SIZE)
+                add_brush_to_values_at(values, rasterPos, BRUSH)
+                raster_must_redraw = True
+        if raster_must_redraw:
+            draw_raster(values, OFFSET, SIZE)
+            pygame.display.update()  # draw the window onto the screen
+    raster_must_redraw = False
